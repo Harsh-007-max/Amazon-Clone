c67b06a95ec4ebdd6ed88da835f379fcb69af6f6
Harsh <harshbhalodiya1209+github@gmail.com>
2023-02-24 01:34:27 +0530

    added firebase setup and created new file firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdfQmHV73Acmh3qelpd1wS7XabocSJ0JU",
+  authDomain: "clone-61584.firebaseapp.com",
+  projectId: "clone-61584",
+  storageBucket: "clone-61584.appspot.com",
+  messagingSenderId: "234470627426",
+  appId: "1:234470627426:web:b5c9ed2ae7064dc50d193b",
+  measurementId: "G-VH5WBYV96M"
+};
